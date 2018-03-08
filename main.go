30627aded0a79823f6fc683489d4e5b6e1a94217
mono <mono@mono0x.net>
2018-03-09 00:30:08 +0900

    Add files

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/lambda"
+	murmur "github.com/mono0x/murmur/lib"
+	"golang.org/x/sync/errgroup"
+)
+
+//go:generate retool do go-assets-builder -o assets.go config
+
+func handler() error {
+	var eg errgroup.Group
+	for _, file := range Assets.Files {
+		file := file
+		eg.Go(func() error {
+			config, err := murmur.LoadConfig(file)
+			if err != nil {
+				return err
+			}
+
+			source, err := config.NewSource()
+			if err != nil {
+				return err
+			}
+
+			sink, err := config.NewSink()
+			if err != nil {
+				return err
+			}
+			defer sink.Close()
+
+			notifier := murmur.NewNotifier(source, sink)
+			return notifier.Notify()
+		})
+	}
+	return eg.Wait()
+}
+
+func main() {
+	lambda.Start(handler)
+}
